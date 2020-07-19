2700db2b4d705033354f4d3b1c3414e975e871a1
smriti29 <smritip29@gmail.com>
2020-07-20 02:53:46 +0530

    Updates

---
+++ precache-manifest.1ea110e32f761da1cafa85c6dd572359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d52868ada62526c846a740c987948e0",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "b51c586d641f9802f059",
+    "url": "/minesweeper/static/css/main.919b9411.chunk.css"
+  },
+  {
+    "revision": "68baaa722db176c834e7",
+    "url": "/minesweeper/static/js/2.9d4ca3f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/minesweeper/static/js/2.9d4ca3f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51c586d641f9802f059",
+    "url": "/minesweeper/static/js/main.dbf63d05.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
